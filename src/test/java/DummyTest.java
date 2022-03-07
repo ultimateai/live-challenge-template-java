5b3385d93b52ca379ad1caff35c3209ad358b987
Yonatan Wilkof <yonatan.wilkof@ultimate.ai>
2022-03-07 12:24:53 +0100

    initial

---
+++ src/test/java/DummyTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class DummyTest {
+    @Test
+    void myTest() {
+        assertThat(true).isTrue();
+    }
+}
